eb188efac49036955ac6f5b38d3169d428abdee3
Aleksander Gielnicki <aleksander.gielnicki@gmail.com>
2024-04-01 20:54:58 +0200

    feat: move card list viewing and editing to a single widget

--- lib/screens/card_list_widget.dart
+++ lib/screens/card_list_widget.dart
@@ -1,7 +1,6 @@
 import 'package:flashcards/db.dart';
 import 'package:flashcards/screens/add_card_widget.dart';
 import 'package:flashcards/screens/card_widget.dart';
-import 'package:flashcards/screens/edit_card_list_widget.dart';
 import 'package:flutter/material.dart';
 
 class CardListWidget extends StatefulWidget {
@@ -19,6 +18,8 @@ class CardListWidget extends StatefulWidget {
 }
 
 class _CardListWidgetState extends State<CardListWidget> {
+  bool _editMode = false;
+
   @override
   Widget build(BuildContext context) {
     final name = widget.collection.name;
@@ -29,52 +30,113 @@ class _CardListWidgetState extends State<CardListWidget> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text('Fiszkeły ze zbioru: $name'),
-      ),
-      body: Column(
-        children: [
-          ListView.builder(
-            itemCount: totalCards,
-            shrinkWrap: true,
-            itemBuilder: (_, index) {
-              return ListTile(
-                title: Text(cards[index].sideAText),
+        actions: [
+          PopupMenuButton(
+            itemBuilder: (BuildContext context) => [
+              PopupMenuItem(
+                child: const Text('Dodaj fiszkełę'),
                 onTap: () => Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => CardWidget(
-                      card: cards[index],
-                      cardIndex: index,
-                      totalCards: totalCards,
-                    ),
+                    builder: (context) =>
+                        AddCardWidget(collectionKey: widget.collectionKey),
                   ),
-                ),
-              );
-            },
-          ),
-          ElevatedButton(
-            onPressed: () => Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) =>
-                    AddCardWidget(collectionKey: widget.collectionKey),
+                ).then((_) => setState(() {})),
               ),
-            ).then((_) => setState(() {})),
-            child: const Text('Dodaj fiszkełe'),
-          ),
-          ElevatedButton(
-            onPressed: () => Navigator.push(
+              PopupMenuItem(
+                  child: !_editMode
+                      ? const Text('Edytuj fiszkeły')
+                      : const Text('Zakończ edycję'),
+                  onTap: () => setState(
+                        () {
+                          _editMode = !_editMode;
+                        },
+                      ))
+            ],
+          )
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: totalCards,
+        itemBuilder: (_, index) {
+          return ListTile(
+            title: Text(cards[index].sideAText),
+            onTap: () => Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) => EditCardListWidget(
-                  collection: widget.collection,
-                  collectionKey: widget.collectionKey,
+                builder: (context) => CardWidget(
+                  card: cards[index],
+                  cardIndex: index,
+                  totalCards: totalCards,
                 ),
               ),
             ),
-            child: const Text('Edytuj fiszkeły'),
-          ),
-        ],
+            trailing: _editMode
+                ? IconButton(
+                    onPressed: () => _deleteCardDialog(
+                      cardIndex: index,
+                      collectionKey: widget.collectionKey,
+                      context: context,
+                      name: cards[index].sideAText,
+                    ),
+                    icon: const Icon(Icons.delete),
+                  )
+                : null,
+          );
+        },
       ),
     );
+  }
+
+  Future<void> _deleteCardDialog(
+      {required int cardIndex,
+      required int collectionKey,
+      required BuildContext context,
+      required String name}) async {
+    return showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Potwierdź wybór'),
+          content: Text('Czy na pewno chcesz usunąć fiszkełę: $name?'),
+          actions: [
+            TextButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: const Text('Nie')),
+            TextButton(
+                onPressed: () {
+                  _deleteCard(
+                    cardIndex: cardIndex,
+                    collectionKey: collectionKey,
+                  );
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Tak'))
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _deleteCard({
+    required int cardIndex,
+    required int collectionKey,
+  }) async {
+    // this looks like a job for db.dart
+    final collection = flashCardCollections.get(collectionKey);
+    final cards = collection?.cards;
+
+    if (cards != null && cards.length > cardIndex) {
+      cards.removeAt(cardIndex);
+      final editedCollection =
+          FlashCardCollection(cards: cards, name: collection!.name);
+
+      await flashCardCollections.put(collectionKey, editedCollection);
+      await flashCardCollections.flush();
+    }
+
+    // this should probably be rewritten using a notifier
+    setState(() {});
   }
 }
